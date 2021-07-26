cf7b2fe247806e6e51617bb4dfea00f9a5b1f3b9
teerut26 <teerutsr@gmail.com>
2021-07-26 12:51:30 +0700

    1.0.0 fix callback

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.config = {
     clientId: '798c6243131e4e8dabc6fbe8f63e8549',
     clientSecret: '39ec6a2c20c44dfc9a5db6bf4d898db8',
-    redirectUri: 'http://localhost:3000/api/callback'
+    redirectUri: 'https://tepify-next-js.vercel.app/api/callback'
 }
